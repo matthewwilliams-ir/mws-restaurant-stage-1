3c681e2b7abb84e09d171bbc31f9744d56e7c8c5
Matthew Williams <matthew.williams@impact.com>
2018-08-21 00:04:44 +0200

    Modify sw to render sites when offline

--- sw.js
+++ sw.js
@@ -1,20 +1,42 @@
-var staticCacheName = 'restaurant-info';
+const staticCacheName = 'restaurant-info-v3';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'restaurant.html',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'sw.js',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+  '/restaurant.html?id=1',
+  '/restaurant.html?id=2',
+  '/restaurant.html?id=3',
+  '/restaurant.html?id=4',
+  '/restaurant.html?id=5',
+  '/restaurant.html?id=6',
+  '/restaurant.html?id=7',
+  '/restaurant.html?id=8',
+  '/restaurant.html?id=9',
+  '/restaurant.html?id=10',
+  'css/styles.css',
+  'data/restaurants.json',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(staticCacheName).then(cache => {
-      return cache.addAll([
-        //cache all static files
-        '/',
-        'index.html',
-        'restaurant.html',
-        'js/dbhelper.js',
-        'js/main.js',
-        'sw.js',
-        'img/',
-        'js/restaurant_info.js',
-        'css/styles.css'
-      ]);
+      return cache.addAll(urlsToCache);
     })
   );
 });
@@ -22,16 +44,16 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
 
   var requestUrl = new URL(event.request.url);
-  if (requestUrl.origin === location.origin) {
-    if (requestUrl.pathname === './restaurant.html') {
-      event.respondWith(caches.match('./restaurant.html'));
-      return;
-    }
+
+  if (requestUrl.origin === "https://api.tiles.mapbox.com") {
+    return;
   }
 
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return response || fetch(event.request, {
+        mode: 'no-cors'
+      });
     })
   );
 
@@ -41,4 +63,20 @@ self.addEventListener('message', event => {
   if (event.data.action === 'skipWaiting') {
     self.skipWaiting();
   }
+});
+
+self.addEventListener('activate', event => {
+  var cacheWhitelist = ['restaurant-info-v3'];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
