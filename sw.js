48cf0ade91b1c8e9542c0e29df6350346e57a96a
Matthew Williams <matthew.williams@impact.com>
2018-08-21 00:06:46 +0200

    Refactor sw code

--- sw.js
+++ sw.js
@@ -45,10 +45,6 @@ self.addEventListener('fetch', event => {
 
   var requestUrl = new URL(event.request.url);
 
-  if (requestUrl.origin === "https://api.tiles.mapbox.com") {
-    return;
-  }
-
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request, {
